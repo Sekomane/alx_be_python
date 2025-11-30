df6ec80ec2f019ca1eca69f638bb048ec6f18c39
Rorisang <130669303+Sekomane@users.noreply.github.com>
2025-11-30 22:53:57 +0200

    Refactor temperature conversion tool for clarity

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -3,11 +3,11 @@
 FAHRENHEIT_TO_CELSIUS_FACTOR=5/9
 CELSIUS_TO_FAHRENHEIT_FACTOR=9/5
 
-def convert_to_celsius(f):
-    return (f-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
+def convert_to_celsius(fahrenheit):
+    return (fahrenheit-32)*FAHRENHEIT_TO_CELSIUS_FACTOR
 
-def convert_to_fahrenheit(c):
-    return c*CELSIUS_TO_FAHRENHEIT_FACTOR+32
+def convert_to_fahrenheit(celsius):
+    return celsius*CELSIUS_TO_FAHRENHEIT_FACTOR+32
 
 def main():
     temp=input("Enter the temperature to convert: ")
